84d8b3d2a2d535eb472c3f7500750f04a9d102c2
Jose Sanchez <josesancheztrujillo90@hotmail.com>
2025-10-30 15:46:41 -0500

    active lenis

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <LenisSmoothScroll /> */}
+        <LenisSmoothScroll />
         <Navbar />
         <main>{children}</main>
       </body>
